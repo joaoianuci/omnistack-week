9cc2c9a93f21e87b59fa894b498a33a340004d57
João Vitor <Joo Vitor Ianuci>
2019-08-12 17:31:14 -0300

    ok

---
+++ back/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-xjfpv.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3331);
+
+// M - Model, V - View, C - Controller
